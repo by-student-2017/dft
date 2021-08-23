3c1485e2cf59030fe42e8f961e12ad19950b6468
by-student-2017 <studentsctest@gmail.com>
2021-08-23 19:42:39 +0900

    ubuntu 20.04 LTS

--- NLDFT_FMT_Slit_beta_v0.9.4/run_openmpi.sh
+++ NLDFT_FMT_Slit_beta_v0.9.4/run_openmpi.sh
@@ -2,9 +2,9 @@
 
 set OMP_NUM_THREADS=1
 
-items=( 0.34 0.38 0.42 0.46 0.50 0.55 0.60 0.69 0.79 0.90
- 1.02 1.18 1.35 1.61 1.93 2.31 2.90 3.63 4.75 6.51 10.0
-)
+#items=( 0.34 0.38 0.42 0.46 0.50 0.55 0.60 0.69 0.79 0.90
+# 1.02 1.18 1.35 1.61 1.93 2.31 2.90 3.63 4.75 6.51 10.0
+#)
 
 if [ ! -e nldft_fmt_slit_openmpi.exe ]; then
 	mpic++ -O2 nldft_fmt_slit_openmpi.cpp -o nldft_fmt_slit_openmpi.exe
@@ -14,7 +14,11 @@ if [ ! -d results ]; then
 	mkdir results
 fi
 
-for w in "${items[@]}"; do
+#for w in "${items[@]}"; do
+for ((i=34;i<=1000;i+=2))
+do
+	w=`echo $i | awk '{printf "%4.2f", $1/100}'`
+	#echo $w
 	cp temp_parameters.txt parameters.txt
 	echo "Pore width = ${w} [nm]"
 	H=`awk -v w=${w} "BEGIN {print w+0.34}"`
