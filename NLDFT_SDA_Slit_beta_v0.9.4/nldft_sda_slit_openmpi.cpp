6f805524753b0117778549ba08177a1e1d77cbd2
by-student-2017 <studentsctest@gmail.com>
2021-08-30 15:17:46 +0900

    ubuntu 20.04 LTS

--- NLDFT_SDA_Slit_beta_v0.9.4/nldft_sda_slit_openmpi.cpp
+++ NLDFT_SDA_Slit_beta_v0.9.4/nldft_sda_slit_openmpi.cpp
@@ -139,7 +139,7 @@ void read_parameters(void){
 	// ---------- ----------- ------------ ------------
 	nstep = int(num[2]);
 	if ( nstep == 0 ) {
-		nstep = int((H-sigma_ss)/0.02 + 0.5);
+		nstep = int((H-sigma_ss)/0.01 + 0.5);
 		if ( nstep%2 == 1 ){
 			nstep = nstep + 1;
 		}
