50b912d942079144632aed82b55337e6e02d789b
by-student-2017 <studentsctest@gmail.com>
2022-12-12 20:43:41 +0900

    ubumtu 18.04 lts

--- Tikhonov/Tikhonov_ridge.py
+++ Tikhonov/Tikhonov_ridge.py
@@ -61,66 +61,80 @@ for p in pp0:
 from sklearn.linear_model import Ridge
 
 #------------------
-ndata = 50
+ndata = 300
 Ly = np.zeros(ndata)
 Lx = np.zeros(ndata)
 Lam = np.zeros(ndata)
 for i in range(ndata):
-	Lam[i] = i*1+4
+	Lam[i] = i*1+1
 	#
 	# self-made
-	#linear = RidgeReg(lambda_ = Lam[i])
+	linear = RidgeReg(lambda_ = Lam[i])
 	#
 	# sklearn
-	linear = Ridge(alpha=Lam[i])
+	#linear = Ridge(alpha=Lam[i])
 	
 	linear.fit(X, y)
 	#print(linear.coef_)
 	Ly[i] = np.log10( np.linalg.norm(linear.coef_, ord=1) )
 	Lx[i] = np.log10( np.linalg.norm((X @ linear.coef_ - y), ord=1) )
 	print(Lam[i], Lx[i], Ly[i])
+	#
+	if np.min(linear.coef_)<0:
+		iLam_x0 = i
 
-Lam_x0 = 200
+Lam_x0 = Lam[iLam_x0]
 #--------------------------------------------------------------------
 import matplotlib.pyplot as plt
+#------------------
 plt.plot(Lx,Ly)
 plt.show()
-
+#------------------
 # self-made
-#linear = RidgeReg(lambda_ = Lam_x0)
+linear = RidgeReg(lambda_ = Lam_x0)
 #
 # sklearn
-linear = Ridge(alpha = Lam_x0)
+#linear = Ridge(alpha = Lam_x0)
 
 linear.fit(X, y)
 #print(linear.coef_)
-
+#------------------
 wdata = len(x[0])-1
+dvdw = np.zeros(wdata)
 a = np.zeros(wdata)
 i = 0
 for ww in dfx[:0]:
 	if not i==0:
 		a[i-1] = float(ww)
+		dvdw[i-1] = linear.coef_[i-1] / a[i-1]
 	i = i + 1
 #
 cw = np.zeros(wdata)
 cw[0] = 0.0
 for i in range(1,wdata):
 	cw[i] = cw[i-1] + linear.coef_[i]
-
+#------------------
+plt.plot(pp0,y, color="blue", label="Exp.")
+plt.plot(pp0,(X @ linear.coef_), c="r", label="Fit(lambda={0})".format(Lam_x0))
+plt.legend()
+plt.xlabel("Relative pressure, P/P0", fontsize=10)
+plt.ylabel("Adsorption, [mmol/g]", fontsize=10)
+plt.xscale('log')
+plt.show()
+#------------------
 fig = plt.figure()
-
 ax1 = fig.subplots()
 ax2 = ax1.twinx()
-
-ax1.plot(a,linear.coef_, color="blue", label="dVdw")
+#ax1.plot(a,linear.coef_, color="blue", label="dVdw")
+ax1.plot(a,dvdw, color="blue", label="dVdw")
 ax2.plot(a, cw, c="r", label="cum")
 ax1.set_xlabel("Pore width, w [nm]", fontsize=10)
-ax1.set_ylabel("Distribution, dV/dw [cm3/g]", fontsize=10)
-ax2.set_ylabel("Volume, cm3/g", fontsize=10)
+#ax1.set_ylabel("Distribution, dV/dw [cm3/g]", fontsize=10)
+ax1.set_ylabel("Distribution, dV/dw [cm3/g/nm]", fontsize=10)
+ax2.set_ylabel("Cumulative Volume, cm3/g", fontsize=10)
 plt.hlines(y=0, xmin=a[0], xmax=a[wdata-1])
 plt.title("Title")
 plt.legend()
 #plt.xscale('log')
-
 plt.show()
+#--------------------------------------------------------------------
